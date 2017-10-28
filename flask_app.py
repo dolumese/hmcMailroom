ecc6a0265919d744a87ec21096b6d9a50ecf5ed9
David Olumese <dolumese@g.hmc.edu>
2017-10-28 07:42:55 +0000

    Added JSON functionality

--- flask_app.py
+++ flask_app.py
@@ -1,11 +1,16 @@
 from flask import Flask
 from flask import request
-from flask import render_template
+from flask import render_template, redirect, url_for
 
 app = Flask(__name__)
 app.config["DEBEUG"] = True
 
-@app.route('/')
+@app.route('/', methods=["GET", "POST"])
 def index():
-    return render_template("index.html")
+    if request.method == "GET":
+        return render_template("index.html")
+
+    data = request.get_json()
+    print(data) # print data to log
+    return 'Successful: JSON data posted'
 
