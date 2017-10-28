f828d12fd69ded55a5dcfd1d0fdc0eb8fe01ec88
David Olumese <dolumese@g.hmc.edu>
2017-10-28 08:07:57 +0000

    Add database set-up

--- flask_app.py
+++ flask_app.py
@@ -1,9 +1,23 @@
 from flask import Flask
 from flask import request
 from flask import render_template, redirect, url_for
+from flask_sqlalchemy import SQLAlchemy
 
 app = Flask(__name__)
 app.config["DEBEUG"] = True
+
+# MySQL Database Set-up
+SQLALCHEMY_DATABASE_URI = "mysql+mysqlconnector://{username}:{password}@{hostname}/{databasename}".format(
+    username="hmcMailroom",
+    password="MaiLlIAm",
+    hostname="hmcMailroom.mysql.pythonanywhere-services.com",
+    databasename="hmcMailroom$doorStatus",
+)
+app.config["SQLALCHEMY_DATABASE_URI"] = SQLALCHEMY_DATABASE_URI
+app.config["SQLALCHEMY_POOL_RECYCLE"] = 299
+app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
+
+db = SQLAlchemy(app)
 
 @app.route('/', methods=["GET", "POST"])
 def index():
